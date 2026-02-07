695d27ee1bb5528eee80b744f2d7ef2ae7d56dbc
sy4016 <sy4016@hw.ac.uk>
2026-02-08 00:08:29 +0400

    Optimised the code, added examples in README

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from groq import Groq
 import os
 from dotenv import load_dotenv
 import matplotlib.pyplot
+import time
+
+start = time.time()
 
 CO2E = 0
 CO2T = 0
@@ -16,7 +19,7 @@ for i in T:
         "DIESEL":[0.14340,0.17174,0.21007,0.17304],"HYBRID":[0.11413,0.11724,0.15650,0.12825]}
         try:
             fuel = input("Enter the fuel used ").upper()
-            dist = float(input("Enter the distance travelled in km "))
+            dist = float(input("Enter the distance travelled in km for car "))
             print("Select car size :-","1. Small","2. Medium","3. Large","4. Average",sep="\n")
             car_type = int(input("Enter 1/2/3/4 "))
             car = dist * fuels[fuel][car_type-1]
@@ -25,12 +28,14 @@ for i in T:
             TD["Car"] = car
         except KeyError:
             print("Fuel Type Not Found")
+            break
         except:
             print("An errror occured, Please try again later.")
+            break
     if i.upper() == "MOTORBIKE":
         emission = {"SMALL":  0.08319,"MEDIUM":  0.10107,"LARGE":  0.13252,"AVERAGE":  0.11367}
         try:
-            dist = float(input("Enter the distance travelled in km "))
+            dist = float(input("Enter the distance travelled in km for motorbike "))
             size = input("Select motorbiker size - Small, Medium, Large, Average ").upper()
             bike = dist * emission[size]
             CO2E +=  bike
@@ -38,30 +43,47 @@ for i in T:
             TD["Bike"] = bike
         except:
             print("An errror occured, Please try again later.")
+            break
     if i.upper() == "BUS":
         try:
-            dist = float(input("Enter the distance travelled in km "))
+            dist = float(input("Enter the distance travelled in km for bus "))
             bus = dist * 0.10385
             CO2E += bus
             CO2T += bus
             TD["Bus"] = bus
         except:
             print("An errror occured, Please try again later.")
+            break
 
 try:
-    elec = float(input("Enter the electricity usage in kWh "))
-    elect = elec * 0.17700
-    CO2E += elect
-    CO2EL += elect
+    elec = input("Enter the electricity usage in kWh ")
+    if elec != "" and elec != "NA":
+        elec = float(elec)
+        elect = elec * 0.17700
+        CO2E += elect
+        CO2EL += elect
 except:
     print("An errror occured, Please try again later.")
         
 try:
-    wat1 = float(input("Enter your water usage in m^3  "))
-    wat2 = float(input("Enter a rough estimate of water disposed in m^3 "))
-    wat = wat1 *0.19130 + wat2 * 0.1708800
-    CO2E += wat
-    CO2W += wat
+    wat1 = input("Enter your water usage in m^3  ")
+    wat2 = input("Enter a rough estimate of water disposed in m^3 ")
+    if (wat1 != "" and wat1 != "NA") and (wat1 != "" and wat1 != "NA"):
+        wat1 = float(wat1)
+        wat2 = float(wat2)
+        wat = wat1 *0.19130 + wat2 * 0.1708800
+        CO2E += wat
+        CO2W += wat
+    elif (wat1 != "" and wat1 != "NA"):
+        wat1 = float(wat1)
+        wat = wat1 *0.19130 
+        CO2E += wat
+        CO2W += wat
+    elif (wat2 != "" and wat2 != "NA"):
+        wat2 = float(wat2)
+        wat = wat2 * 0.1708800
+        CO2E += wat
+        CO2W += wat
 except:
     print("An errror occured, Please try again later.")
 
@@ -71,31 +93,45 @@ co2_1 = []
 for veh,fp in TD.items():
     act.append(veh)
     co2_1.append(fp)
-act.extend(["Electricity","Water"])
-co2_1.extend([CO2EL,CO2W])
+if CO2EL != 0:
+    act.append("Electricity")
+    co2_1.append(CO2EL)
+if CO2W != 0:
+    act.append("Water")
+    co2_1.append(CO2W)
+
 matplotlib.pyplot.bar(act,co2_1)
 matplotlib.pyplot.xlabel("Daily Practices")
 matplotlib.pyplot.ylabel("CO2 Footprint")
 matplotlib.pyplot.show()
 
-cat = ["Transport","Electricity","Water"]
-co2_2 = [CO2T,CO2EL,CO2W]
+cat = []
+co2_2 = []
+if CO2T != 0:
+    cat.append("Transport")
+    co2_2.append(CO2T)
+if CO2EL != 0:
+    cat.append("Electricity")
+    co2_2.append(CO2EL)
+if CO2W != 0:
+    cat.append("Water")
+    co2_2.append(CO2W)
 matplotlib.pyplot.pie(co2_2, labels = cat, autopct='%0.2f%%',startangle=90 )
 matplotlib.pyplot.show()
 
-load_dotenv()
-api_key = os.getenv("APIKey")
-if not api_key:
-    raise ValueError("API Key not found in .env file")
+try:
+    load_dotenv()
+    api_key = os.getenv("APIKey")
+    if not api_key:
+       print("API key missing")
 
+    client = Groq(api_key=api_key)
 
-client = Groq(api_key=api_key)
+    chat = client.chat.completions.create(model="llama-3.1-8b-instant", 
+    messages=[{"role": "user", "content": "My carbon footprint is {} kg of CO2 for 1 week - Transport - {}; Electricity - {}, Water - {}, can you explain what it means also provide me with alternate practices to reduce it, I want 6 realistic alternatives to reduce it with priority for the biggest contributor and Give qualitative impact only: High/Medium/Low".format(CO2E,CO2T,CO2EL,CO2W) }])
+    print(chat.choices[0].message.content)
 
-chat = client.chat.completions.create(model="llama-3.1-8b-instant", 
-messages=[{"role": "user", "content": "My carbon footprint is {} kg of CO2 for 1 week - Transport - {}; Electricity - {}, Water - {}, can you explain what it means".format(CO2E,CO2T,CO2EL,CO2W) }])
-print(chat.choices[0].message.content)
-    
-chat = client.chat.completions.create(model="llama-3.1-8b-instant",
-messages=[{"role": "user", "content": "My carbon footprint is {} for 1 week - Transport - {}; Electricity - {}, Water - {}, can you provide me with alternate practices to reduce it, I want 6 realistic alternatives to reduce it with priority fore the biggest contributor and give estimated impact only if confident".format(CO2E,CO2T,CO2EL,CO2W) }])
-print(chat.choices[0].message.content)
+except:
+    print("AI functionality is not available right now, Please try again later")
 
+print("latency =",time.time()-start)
